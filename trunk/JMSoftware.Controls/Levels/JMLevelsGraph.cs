374b68a4f116b7ea9eca2763da0dba8e18e135ed
wardog_uk <wardog_uk@a846db28-e15e-4f3f-a9c7-af426bbe50df>
2011-02-17 15:05:51 +0000

    Move the levels stuff into to the controls dll

--- trunk/JMSoftware.Controls/Levels/JMLevelsGraph.cs
+++ trunk/JMSoftware.Controls/Levels/JMLevelsGraph.cs
@@ -23,7 +23,7 @@
 //    along with this program.  If not, see http://www.gnu.org/licenses/.
 // </license>
 //---------------------------------------------------------------------------------------
-namespace JMSoftware.CustomControl.Levels
+namespace JMSoftware.Controls.Levels
 {
     using System.Drawing;
     using System.Windows.Forms;
